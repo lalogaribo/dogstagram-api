2db91bfc24e4cbcc31de223057d7e99460c1ab88
caicindy87 <caicindy87@gmail.com>
2020-05-05 14:16:37 -0700

    Try to use cookie to create new dog and post

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,11 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'localhost:3000/api/v1/dogs', 'localhost:3000/api/v1/posts'
 
     resource '*',
       headers: :any,
+      credentials: true,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :dogs, only: [:create]
-      resources :posts
+      resources :dogs, only: [:create] do
+        resources :posts
+      end
     end
   end
 end
